ab46d80183ddc8baf2eccd64bea02f077d208c04
lmiano <lmiano@doxee.com>
2024-05-31 18:01:59 +0200

    anadido comentario ejercicio 6

--- dispositivo/src/dispositivo/interfaces/Configuracion.java
+++ dispositivo/src/dispositivo/interfaces/Configuracion.java
@@ -6,6 +6,11 @@ public interface Configuracion {
 	public static final String M2MIO_PASSWORD_MD5 = "<m2m.io password (MD5 sum of password)>";
 	
 //	public static final String TOPIC_BASE = "es/upv/inf/muiinf/ina/";
+
+	/**
+	 * La variable TOPIC_BASE se usa como una base común o prefijo para construir otros temas relacionados. Esto facilita la organización y el
+	 * manejo de nombres de topicos de manera coherente y evita la repetición de cadenas de texto en diferentes partes del código.
+	 */
 	public static final String TOPIC_BASE = "";
 	public static final String TOPIC_REGISTRO =  Configuracion.TOPIC_BASE + "gestion/dispositivos";
 
